a6127d308a46005ee8e43c15e422a7718b472242
Shanmathuran Sritharan <shanmathuran.sritharan@xovis.com>
2017-12-17 23:47:28 +0100

    remove app title

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
     constructor(props)
     {
         super(props);
-        
+
     }
 
     componentDidMount() {
@@ -32,7 +32,6 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
 
 
